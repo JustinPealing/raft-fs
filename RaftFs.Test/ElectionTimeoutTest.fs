3ecc6faeffe2840a01e251414df3df8f9608194a
Justin Pealing <justin@pealing.me.uk>
2017-11-09 19:46:34 +0000

    Unit test election timeout

---
+++ RaftFs.Test/ElectionTimeoutTest.fs
@@ -0,0 +1,22 @@
+namespace RaftFs.Test
+
+open System
+open NUnit.Framework
+open RaftFs
+
+module ElectionTimeoutTest = 
+    
+    /// <summary>
+    /// With no other interaction an election timeout should trigger after the given time interval.
+    /// </summary>
+    [<Test>]
+    let ``Election timeout``() = Async.RunSynchronously <| async {
+        let start = DateTime.UtcNow
+        let mutable endTime = DateTime.MinValue
+        let callback() =
+            endTime <- DateTime.UtcNow
+        Elections.startElectionTimeout (TimeSpan.FromMilliseconds 100.0) callback
+        Assert.AreEqual(DateTime.MinValue, endTime)
+        do! Async.Sleep 110
+        Assert.IsTrue((endTime - start).TotalMilliseconds > 0.0)
+    }
